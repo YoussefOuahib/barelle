6bc3620a6eead27716df6f6d0d4851784adac5c8
spectreOX <youssefouahib98@gmail.com>
2022-09-03 17:10:42 +0100

    fix error

--- app/Traits/UploadTrait.php
+++ app/Traits/UploadTrait.php
@@ -10,6 +10,7 @@ trait UploadTrait {
     public function upload($request) : string {
         $imageName = Carbon::now()->timestamp . '.' . $request->image->extension();
         $location = public_path('storage/images/' . $imageName);
+        Image::configure(array('driver' => 'imagick'));
         Image::make($request->image)->resize(600, 600)->save($location);
 
         return $imageName;
